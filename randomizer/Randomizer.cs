542b8d41bae1e393d7f3fb280777c84a8ddf0e9e
sigmasin <tksstepan@gmail.com>
2017-06-15 14:55:11 -0600

    fix mapstone edge cases

--- randomizer/Randomizer.cs
+++ randomizer/Randomizer.cs
@@ -21,36 +21,36 @@ public static class Randomizer
 		if (File.Exists("randomizer.dat"))
 		{
 			string[] array = File.ReadAllLines("randomizer.dat");
-			string[] flags = array[0].Split(new char[]
+			string[] array2 = array[0].Split(new char[]
 			{
 				','
 			});
-			for (int i = 0; i < flags.Length; i++)
+			for (int i = 0; i < array2.Length; i++)
 			{
-				if (flags[i].ToLower() == "ohko")
+				if (array2[i].ToLower() == "ohko")
 				{
 					Randomizer.OHKO = true;
 				}
-				if (flags[i].ToLower() == "0xp")
+				if (array2[i].ToLower() == "0xp")
 				{
 					Randomizer.ZeroXP = true;
 				}
-				if (flags[i].ToLower() == "nobonus")
+				if (array2[i].ToLower() == "nobonus")
 				{
 					Randomizer.BonusActive = false;
 				}
 			}
 			for (int j = 1; j < array.Length; j++)
 			{
-				string[] array2 = array[j].Split(new char[]
+				string[] array3 = array[j].Split(new char[]
 				{
 					'|'
 				});
 				int num;
-				int.TryParse(array2[0], out num);
+				int.TryParse(array3[0], out num);
 				int num2;
-				int.TryParse(array2[2], out num2);
-				Randomizer.Table[num] = new RandomizerAction(array2[1], num2);
+				int.TryParse(array3[2], out num2);
+				Randomizer.Table[num] = new RandomizerAction(array3[1], num2);
 			}
 		}
 	}
@@ -58,7 +58,7 @@ public static class Randomizer
 	// Token: 0x06003738 RID: 14136
 	public static void getPickup()
 	{
-		RandomizerSwitch.GivePickup((RandomizerAction)Randomizer.Table[Randomizer.resolvePosition()]);
+		Randomizer.getPickup(Characters.Sein.Position);
 	}
 
 	// Token: 0x06003739 RID: 14137
@@ -78,29 +78,6 @@ public static class Randomizer
 	{
 		Randomizer.MessageProvider.SetMessage(message);
 		UI.Hints.Show(Randomizer.MessageProvider, HintLayer.GameSaved, 3f);
-	}
-
-	// Token: 0x0600373C RID: 14140
-	public static int resolvePosition()
-	{
-		int num = (int)(Math.Floor((double)Characters.Sein.Position.x / Randomizer.GridFactor) * Randomizer.GridFactor * 10000.0 + Math.Floor((double)Characters.Sein.Position.y / Randomizer.GridFactor) * Randomizer.GridFactor);
-		for (int i = -1; i <= 1; i++)
-		{
-			for (int j = -1; j <= 1; j++)
-			{
-				if (Randomizer.Table.ContainsKey(num + (int)Randomizer.GridFactor * (10000 * i + j)))
-				{
-					return num + (int)Randomizer.GridFactor * (10000 * i + j);
-				}
-			}
-		}
-		RandomizerMessageProvider arg_AE_0 = Randomizer.MessageProvider;
-		Vector3 position = Characters.Sein.Position;
-		string str = position.x.ToString();
-		string str2 = ", ";
-		position = Characters.Sein.Position;
-		Randomizer.showHint("Error finding pickup at " + str + str2 + position.y.ToString());
-		return num;
 	}
 
 	// Token: 0x0600373D RID: 14141
@@ -157,6 +134,17 @@ public static class Randomizer
 				}
 			}
 		}
+		for (int k = -2; k <= 2; k += 4)
+		{
+			for (int l = -1; l <= 1; l++)
+			{
+				if (Randomizer.Table.ContainsKey(num + (int)Randomizer.GridFactor * (10000 * k + l)))
+				{
+					RandomizerSwitch.GivePickup((RandomizerAction)Randomizer.Table[num + (int)Randomizer.GridFactor * (10000 * k + l)]);
+					return;
+				}
+			}
+		}
 		Randomizer.showHint("Error finding pickup at " + ((int)position.x).ToString() + ", " + ((int)position.y).ToString());
 	}
 
@@ -182,12 +170,12 @@ public static class Randomizer
 	// Token: 0x04003226 RID: 12838
 	public static RandomizerMessageProvider MessageProvider;
 
-	// Token: 0x04003236 RID: 12854
+	// Token: 0x04003227 RID: 12839
 	public static bool OHKO;
 
-	// Token: 0x04003237 RID: 12855
+	// Token: 0x04003228 RID: 12840
 	public static bool ZeroXP;
 
-	// Token: 0x04003244 RID: 12868
+	// Token: 0x04003229 RID: 12841
 	public static bool BonusActive;
 }
