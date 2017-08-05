3a762dce195c950deeed4fe4ba052129ad705d81
sigmasin <tksstepan@gmail.com>
2017-08-05 15:27:47 -0600

    batch file update

--- seed_gen/seed_batch.bat
+++ seed_gen/seed_batch.bat
@@ -2,76 +2,85 @@
 mkdir seeds
 mkdir spoilers
 
-mkdir seeds/casual
-mkdir spoilers/casual
+mkdir seeds\casual
+mkdir spoilers\casual
 
 python seed_generator.py --logic casual --count 99
 move randomizer*.dat seeds/casual/
 move spoiler* spoilers/casual/
 
-mkdir seeds/casual-shards
-mkdir spoilers/casual-shards
+mkdir seeds\casual-shards
+mkdir spoilers\casual-shards
 
 python seed_generator.py --logic casual --shards --count 99
 move randomizer*.dat seeds/casual-shards/
 move spoiler* spoilers/casual-shards/
 
-mkdir seeds/casual-limitkeys
-mkdir spoilers/casual-limitkeys
+mkdir seeds\casual-limitkeys
+mkdir spoilers\casual-limitkeys
 
 python seed_generator.py --logic casual --limitkeys --count 99
 move randomizer*.dat seeds/casual-limitkeys/
 move spoiler* spoilers/casual-limitkeys/
 
-mkdir seeds/normal
-mkdir spoilers/normal
+mkdir seeds\normal
+mkdir spoilers\normal
 
 python seed_generator.py --logic normal --count 999 --force-trees
 move randomizer*.dat seeds/normal/
 move spoiler* spoilers/normal/
 
-mkdir seeds/normal-shards
-mkdir spoilers/normal-shards
+mkdir seeds\normal-shards
+mkdir spoilers\normal-shards
 
 python seed_generator.py --logic normal --shards --count 999 --force-trees
 move randomizer*.dat seeds/normal-shards/
 move spoiler* spoilers/normal-shards/
 
-mkdir seeds/normal-limitkeys
-mkdir spoilers/normal-limitkeys
+mkdir seeds\normal-limitkeys
+mkdir spoilers\normal-limitkeys
 
 python seed_generator.py --logic normal --limitkeys --count 999 --force-trees
 move randomizer*.dat seeds/normal-limitkeys/
 move spoiler* spoilers/normal-limitkeys/
 
-mkdir seeds/extended
-mkdir spoilers/extended
+mkdir seeds\extended
+mkdir spoilers\extended
 
 python seed_generator.py --logic extended --count 999 --force-trees
 move randomizer*.dat seeds/extended/
 move spoiler* spoilers/extended/
 
-mkdir seeds/extended-shards
-mkdir spoilers/extended-shards
+mkdir seeds\extended-shards
+mkdir spoilers\extended-shards
 
 python seed_generator.py --logic extended --count 999 --shards --force-trees
 move randomizer*.dat seeds/extended-shards/
 move spoiler* spoilers/extended-shards/
 
-mkdir seeds/extended-limitkeys
-mkdir spoilers/extended-limitkeys
+mkdir seeds\extended-limitkeys
+mkdir spoilers\extended-limitkeys
 
 python seed_generator.py --logic extended --count 999 --limitkeys --force-trees
 move randomizer*.dat seeds/extended-limitkeys/
 move spoiler* spoilers/extended-limitkeys/
 
+mkdir seeds\hard
+mkdir spoilers\hard
+
 python seed_generator.py --logic hard --count 99 --hard --exp-pool 5000
 move randomizer*.dat seeds/hard/
 move spoiler* spoilers/hard/
 
+mkdir seeds\ohko
+mkdir spoilers\ohko
+
 python seed_generator.py --logic ohko --count 99 --ohko --hard --exp-pool 5000
 move randomizer*.dat seeds/ohko/
 move spoiler* spoilers/ohko/
+
+mkdir seeds\0xp
+mkdir spoilers\0xp
 
 python seed_generator.py --logic 0xp --count 99 --hard --zeroxp --noplants
 move randomizer*.dat seeds/0xp/
