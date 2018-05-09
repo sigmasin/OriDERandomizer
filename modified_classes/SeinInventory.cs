23048632fdea1bd1b1d3743292023e16a317f351
turntekGodhead <ari.schumer@gmail.com>
2018-05-08 19:00:59 -0700

    (allegedly) fixes the SA/checkpoint save bug. Big thanks to skul and gecko for finding it

--- modified_classes/SeinInventory.cs
+++ modified_classes/SeinInventory.cs
@@ -1,32 +1,32 @@
 using System;
 using System.Collections.Generic;
 
-// Token: 0x0200032C RID: 812
+// Token: 0x0200032D RID: 813
 public class SeinInventory : SaveSerialize
 {
-	// Token: 0x06001186 RID: 4486
+	// Token: 0x06001188 RID: 4488
 	public SeinInventory()
 	{
 		this.RandomizerItems = new Dictionary<int, int>();
-		this.ItemsAtLastSave = new Dictionary<int, int>();
+		this.ItemsToRevert = new Dictionary<int, int>();
 	}
 
 	// Token: 0x14000015 RID: 21
-	// (add) Token: 0x06001187 RID: 4487
-	// (remove) Token: 0x06001188 RID: 4488
-	public event Action OnCollectKeystones = delegate()
+	// (add) Token: 0x06001189 RID: 4489
+	// (remove) Token: 0x0600118A RID: 4490
+	public event Action OnCollectKeystones = delegate
 	{
 	};
 
 	// Token: 0x14000016 RID: 22
-	// (add) Token: 0x06001189 RID: 4489
-	// (remove) Token: 0x0600118A RID: 4490
-	public event Action OnCollectMapstone = delegate()
+	// (add) Token: 0x0600118B RID: 4491
+	// (remove) Token: 0x0600118C RID: 4492
+	public event Action OnCollectMapstone = delegate
 	{
 	};
 
 	// Token: 0x170002F6 RID: 758
-	// (get) Token: 0x0600118B RID: 4491
+	// (get) Token: 0x0600118D RID: 4493
 	public bool HasKeystones
 	{
 		get
@@ -36,7 +36,7 @@ public class SeinInventory : SaveSerialize
 	}
 
 	// Token: 0x170002F7 RID: 759
-	// (get) Token: 0x0600118C RID: 4492
+	// (get) Token: 0x0600118E RID: 4494
 	public bool HasMapstones
 	{
 		get
@@ -45,13 +45,13 @@ public class SeinInventory : SaveSerialize
 		}
 	}
 
-	// Token: 0x0600118D RID: 4493
+	// Token: 0x0600118F RID: 4495
 	public bool CanAfford(int cost)
 	{
 		return this.Keystones >= cost;
 	}
 
-	// Token: 0x0600118E RID: 4494
+	// Token: 0x06001190 RID: 4496
 	public void SpendKeystones(int cost)
 	{
 		this.Keystones -= cost;
@@ -61,7 +61,7 @@ public class SeinInventory : SaveSerialize
 		}
 	}
 
-	// Token: 0x0600118F RID: 4495
+	// Token: 0x06001191 RID: 4497
 	public void SpendMapstone(int cost)
 	{
 		this.MapStones -= cost;
@@ -71,27 +71,27 @@ public class SeinInventory : SaveSerialize
 		}
 	}
 
-	// Token: 0x06001190 RID: 4496
+	// Token: 0x06001192 RID: 4498
 	public void CollectKeystones(int amount)
 	{
 		this.Keystones += amount;
 		this.OnCollectKeystones();
 	}
 
-	// Token: 0x06001191 RID: 4497
+	// Token: 0x06001193 RID: 4499
 	public void CollectMapstone(int amount)
 	{
 		this.MapStones += amount;
 		this.OnCollectMapstone();
 	}
 
-	// Token: 0x06001192 RID: 4498
+	// Token: 0x06001194 RID: 4500
 	public void RestoreKeystones(int amount)
 	{
 		this.CollectKeystones(amount);
 	}
 
-	// Token: 0x06001193 RID: 4499
+	// Token: 0x06001195 RID: 4501
 	public override void Serialize(Archive ar)
 	{
 		ar.Serialize(ref this.Keystones);
@@ -100,13 +100,17 @@ public class SeinInventory : SaveSerialize
 		ar.Serialize(ref this.RandomizerItems);
 	}
 
-	// Token: 0x060037DB RID: 14299
+	// Token: 0x06001196 RID: 4502
 	public void SetRandomizerItem(int code, int value)
 	{
+		if (!this.ItemsToRevert.ContainsKey(code))
+		{
+			this.ItemsToRevert[code] = this.GetRandomizerItem(code);
+		}
 		this.RandomizerItems[code] = value;
 	}
 
-	// Token: 0x060037DD RID: 14301
+	// Token: 0x06001197 RID: 4503
 	public int GetRandomizerItem(int code)
 	{
 		if (this.RandomizerItems.ContainsKey(code))
@@ -116,17 +120,29 @@ public class SeinInventory : SaveSerialize
 		return 0;
 	}
 
-	// Token: 0x06003842 RID: 14402
+	// Token: 0x06001198 RID: 4504
 	public void IncRandomizerItem(int code, int value)
 	{
+		if (!this.ItemsToRevert.ContainsKey(code))
+		{
+			this.ItemsToRevert[code] = this.GetRandomizerItem(code);
+		}
 		this.RandomizerItems[code] = this.GetRandomizerItem(code) + value;
 	}
 
-	public void OnSave() {
-		 this.ItemsAtLastSave = new Dictionary<int,int>(this.RandomizerItems);
+	// Token: 0x06001199 RID: 4505
+	public void OnSave()
+	{
+		this.ItemsToRevert.Clear();
 	}
-	public void OnDeath() {
-		 this.RandomizerItems = new Dictionary<int,int>(this.ItemsAtLastSave);
+
+	// Token: 0x0600119A RID: 4506
+	public void OnDeath()
+	{
+		foreach (int code in this.ItemsToRevert.Keys)
+		{
+			this.RandomizerItems[code] = this.ItemsToRevert[code];
+		}
 	}
 
 	// Token: 0x04001098 RID: 4248
@@ -138,9 +154,9 @@ public class SeinInventory : SaveSerialize
 	// Token: 0x0400109A RID: 4250
 	public int SkillPointsCollected;
 
-	// Token: 0x040032A3 RID: 12963
-	public Dictionary<int, int> ItemsAtLastSave;
-
-	// Token: 0x040032A3 RID: 12963
+	// Token: 0x0400109D RID: 4253
 	public Dictionary<int, int> RandomizerItems;
+
+	// Token: 0x040032C1 RID: 12993
+	public Dictionary<int, int> ItemsToRevert;
 }
