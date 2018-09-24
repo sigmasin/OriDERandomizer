d9a0fc94d73a9d1f7b27bcd15f6e67196df72be9
Vinay Sarpeshkar <vsarpeshkar@gmail.com>
2018-09-23 19:54:58 -0700

    Implement Ori World Tour seed generation. - Place relics in random zone locations at the start. - Ensure compatibility with limitkeys placements. - Draw relic descriptions from a new module.

---
+++ seed_gen/relics.py
@@ -0,0 +1,90 @@
+from collections import OrderedDict
+
+_glades = [
+	("Unusually Sharp Spike", "Twice as deadly as the other spikes."),
+	("Discarded Leaf", "Seems to have three puncture holes in it."),
+]
+
+_grove = [
+	("Grove Relic 1", "Description of the relic."),
+	("Grove Relic 2", "Description of the relic."),
+	("Grove Relic 3", "Description of the relic."),
+	("Grove Relic 4", "Description of the relic."),
+]
+
+_grotto = [
+	("Grotto Relic 1", "Description of the relic."),
+	("Grotto Relic 2", "Description of the relic."),
+	("Grotto Relic 3", "Description of the relic."),
+	("Grotto Relic 4", "Description of the relic."),
+]
+
+_blackroot = [
+	("Blackroot Relic 1", "Description of the relic."),
+	("Blackroot Relic 2", "Description of the relic."),
+	("Blackroot Relic 3", "Description of the relic."),
+	("Blackroot Relic 4", "Description of the relic."),
+]
+
+_swamp = [
+	("Swamp Relic 1", "Description of the relic."),
+	("Swamp Relic 2", "Description of the relic."),
+	("Swamp Relic 3", "Description of the relic."),
+	("Swamp Relic 4", "Description of the relic."),
+]
+
+_ginso = [
+	("Ginso Relic 1", "Description of the relic."),
+	("Ginso Relic 2", "Description of the relic."),
+	("Ginso Relic 3", "Description of the relic."),
+	("Ginso Relic 4", "Description of the relic."),
+]
+
+_valley = [
+	("Valley Relic 1", "Description of the relic."),
+	("Valley Relic 2", "Description of the relic."),
+	("Valley Relic 3", "Description of the relic."),
+	("Valley Relic 4", "Description of the relic."),
+]
+
+_misty = [
+	("Misty Relic 1", "Description of the relic."),
+	("Misty Relic 2", "Description of the relic."),
+	("Misty Relic 3", "Description of the relic."),
+	("Misty Relic 4", "Description of the relic."),
+]
+
+_forlorn = [
+	("Forlorn Relic 1", "Description of the relic."),
+	("Forlorn Relic 2", "Description of the relic."),
+	("Forlorn Relic 3", "Description of the relic."),
+	("Forlorn Relic 4", "Description of the relic."),
+]
+
+_sorrow = [
+	("Sorrow Relic 1", "Description of the relic."),
+	("Sorrow Relic 2", "Description of the relic."),
+	("Sorrow Relic 3", "Description of the relic."),
+	("Sorrow Relic 4", "Description of the relic."),
+]
+
+_horu = [
+	("Horu Relic 1", "Description of the relic."),
+	("Horu Relic 2", "Description of the relic."),
+	("Horu Relic 3", "Description of the relic."),
+	("Horu Relic 4", "Description of the relic."),
+]
+
+relics = OrderedDict([
+	("Glades", _glades),
+	("Grove", _grove),
+	("Grotto", _grotto),
+	("Blackroot", _blackroot),
+	("Swamp", _swamp),
+	("Ginso", _ginso),
+	("Valley", _valley),
+	("Misty", _misty),
+	("Forlorn", _forlorn),
+	("Sorrow", _sorrow),
+	("Horu", _horu)
+])
