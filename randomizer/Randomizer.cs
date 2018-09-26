b350a9f398d321c3dff5ac92409ea3ec38a3e292
sigmasin <tksstepan@gmail.com>
2018-09-25 22:13:40 -0600

    reset input locks if softlocked at the bottom of gumo's hideout due to cutscene order

--- randomizer/Randomizer.cs
+++ randomizer/Randomizer.cs
@@ -66,6 +66,7 @@ public static class Randomizer
 		Randomizer.fragsEnabled = false;
 		Randomizer.MoveNightBerry = false;
 		Randomizer.WarpCheckCounter = 60;
+		Randomizer.LockedCount = 0;
 		Randomizer.HoruScene = "";
 		Randomizer.HoruMap = new Hashtable();
 		Randomizer.HoruMap["mountHoruStomperSystemsR"] = 60;
@@ -813,6 +814,18 @@ public static class Randomizer
 				}
 				Characters.Sein.Position = new Vector3(750f, -120f);
 			}
+			if (!Characters.Sein.Controller.CanMove && Scenes.Manager.CurrentScene.Scene == "moonGrottoGumosHideoutB")
+			{
+				Randomizer.LockedCount++;
+				if (Randomizer.LockedCount >= 3)
+				{
+					GameController.Instance.ResetInputLocks();
+				}
+			}
+			else
+			{
+				Randomizer.LockedCount = 0;
+			}
 			Randomizer.WarpCheckCounter = 60;
 		}
 	}
@@ -955,4 +968,7 @@ public static class Randomizer
 
 	// Token: 0x040032F7 RID: 13047
 	public static ArrayList GladesData;
+
+	// Token: 0x04003304 RID: 13060
+	public static int LockedCount;
 }
