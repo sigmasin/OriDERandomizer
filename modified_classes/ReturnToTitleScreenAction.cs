df1e29937ef8017aed308ca110fcd2d4b92d966f
Eiko <ari.schumer@gmail.com>
2018-12-15 10:53:43 -0800

    parse seed flag line in return-to-title if it happens during the ginso escape cutscene.

--- modified_classes/ReturnToTitleScreenAction.cs
+++ modified_classes/ReturnToTitleScreenAction.cs
@@ -6,6 +6,17 @@ public class ReturnToTitleScreenAction : ActionMethod
 	// Token: 0x060003A5 RID: 933
 	public override void Perform(IContext context)
 	{
+		if(Randomizer.NeedGinsoEscapeCleanup) {
+			try
+			{
+				Randomizer.ParseFlags(Randomizer.SeedMeta.Split(new char[] {'|'})[0].Split(new char[] {','}));
+			}
+			catch (Exception e)
+			{
+				Randomizer.LogError("ReturnToTitleScreenAction: " + e.Message);
+			}
+			NeedGinsoEscapeCleanup = false;
+		}
 		Randomizer.Returning = false;
 		Randomizer.Warping = 0;
 		RandomizerStatsManager.OnReturnToMenu();
