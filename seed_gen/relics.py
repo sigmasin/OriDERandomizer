370c113ac5bb2c754489cb668be8da8be737a9d5
Vinay Sarpeshkar <vsarpeshkar@gmail.com>
2018-12-22 17:44:07 -0800

    Many new relics.

--- seed_gen/relics.py
+++ seed_gen/relics.py
@@ -3,19 +3,21 @@ from collections import OrderedDict
 _glades = [
 	("Unusually Sharp Spike", "Twice as deadly as the other spikes."),
 	("Discarded Leaf", "Seems to have three puncture holes in it."),
-	("Glades Relic 3", "Description of the relic."),
-	("Glades Relic 4", "Description of the relic."),
+	("Hardy Tuber", "Seems to thrive in the moisture of the Glades."),
+	("Withered Fruit", "Gazing at it evokes memories of happier times."),
+	("Fil's Bracelet", "A simple band made of tightly-woven plant fibers."),
+	("Redcap Mushroom", "Eating these is said to help you grow taller."),
 ]
 
 _grove = [
 	("Unhatched Spider Egg", "Hopefully it stays unhatched."),
-	("Grove Relic 2", "Description of the relic."),
-	("Grove Relic 3", "Description of the relic."),
-	("Grove Relic 4", "Description of the relic."),
+	("Fallen Branch", "A small, faintly glowing branch of the Spirit Tree."),
+	("Reem's Lucky Coin", "Said to help you escape the notice of predators."),
+	("Seed Stash", "An innocent squirrel was saving these. What kind of devil are you?"),
 ]
 
 _grotto = [
-	("Grotto Relic 1", "Description of the relic."),
+	("Slick Stone", "So smooth and slippery, you can barely hang on to it."),
 	("Grotto Relic 2", "Description of the relic."),
 	("Grotto Relic 3", "Description of the relic."),
 	("Grotto Relic 4", "Description of the relic."),
@@ -24,15 +26,16 @@ _grotto = [
 _blackroot = [
 	("Sol's Defused Grenade", "Safe enough to use as a ball! ...right?"),
 	("Torn Friendship Bracelet", "A bond that was made would soon be dissolved."),
-	("Eki's Boots of Fleetness", "He moved swifter than the wind."),
-	("Blackroot Relic 4", "Description of the relic."),
+	("Ike's Boots of Fleetness", "He moved swifter than the wind."),
+	("Naru's Chisel", "A skilled artisan could sculpt great works with this tool."),
+	("Glowing Mushroom", "Doubles as a light source and a tasty snack."),
 ]
 
 _swamp = [
 	("Polluted Water Canteen", "Who would want to drink this?"),
-	("Swamp Relic 2", "Description of the relic."),
-	("Swamp Relic 3", "Description of the relic."),
-	("Swamp Relic 4", "Description of the relic."),
+	("Gold-eyed Frog", "Insects stand no chance against its deft tongue."),
+	("Rhino Fossil", "The hard armor plating of a much smaller ancestor creature."),
+	("Ilo's Training Weights", "Solid rock, nearly too heavy to carry."),
 ]
 
 _ginso = [
@@ -44,8 +47,8 @@ _ginso = [
 
 _valley = [
 	("Treasure Map", "A map depicting a treasure found after a long swim."),
-	("Valley Relic 2", "Description of the relic."),
-	("Valley Relic 3", "Description of the relic."),
+	("White Raven Feather", "A bit too small to be used as a parachute."),
+	("", "Description of the relic."),
 	("Valley Relic 4", "Description of the relic."),
 ]
 
